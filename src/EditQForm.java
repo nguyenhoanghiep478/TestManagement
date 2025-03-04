3221d0a6db17a294c1d8b3b34d1b0d13e318fdcd
nguyenviethoang2205 <96973433+nguyenviethoang2205@users.noreply.github.com>
2025-03-05 02:35:59 +0700

    new

---
+++ src/EditQForm.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class EditQForm {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JButton button1;
+    private JButton button2;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+}
---
+++ src/QuestFrom.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class QuestFrom {
+    private JTable table1;
+    private JButton importButton;
+    private JButton editButton;
+    private JButton searchButton;
+}
